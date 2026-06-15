3c9777d8ccf3281deb09bd119d5f2980f964cdfa
Raja Shehroz <r.m.shehroz047@gmail.com>
2026-06-15 16:19:13 +0500

    abayyyy

--- backend/config/firebaseAdmin.js
+++ backend/config/firebaseAdmin.js
@@ -1,3 +1,16 @@
+import admin from "firebase-admin";
+
 const serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT);
 
 serviceAccount.private_key = serviceAccount.private_key.replace(/\\n/g, "\n");
+
+if (!admin.apps.length) {
+  admin.initializeApp({
+    credential: admin.credential.cert(serviceAccount),
+  });
+}
+
+// EXPORT WHAT YOU NEED
+export const auth = admin.auth();
+export const db = admin.firestore();
+export default admin;
