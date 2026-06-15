c772915509cb0f96e268ae224fe6ae84df2bb595
Raja Shehroz <r.m.shehroz047@gmail.com>
2026-06-15 16:40:03 +0500

    kiiiii

--- src/components/Pricing.jsx
+++ src/components/Pricing.jsx
@@ -1,6 +1,7 @@
 import { useState, useEffect } from "react";
 import { auth } from "../services/firebase";
 import "./Pricing.css";
+import { API_BASE } from "../config";
 
 const API_BASE = "fetch(`${API_BASE}"; 
 
