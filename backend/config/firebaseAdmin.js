6ee1f4c415a40a603f5dc30c86dabdfce94e96bd
Raja Shehroz <r.m.shehroz047@gmail.com>
2026-06-15 16:15:24 +0500

    ko

--- backend/config/firebaseAdmin.js
+++ backend/config/firebaseAdmin.js
@@ -1,15 +1,3 @@
-import admin from "firebase-admin";
-import fs from "fs";
+const serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT);
 
-const serviceAccount = JSON.parse(
-  fs.readFileSync(new URL("../firebase-service-account.json", import.meta.url))
-);
-
-if (!admin.apps.length) {
-  admin.initializeApp({
-    credential: admin.credential.cert(serviceAccount),
-  });
-}
-
-export const auth = admin.auth();
-export const db = admin.firestore();
+serviceAccount.private_key = serviceAccount.private_key.replace(/\\n/g, "\n");
