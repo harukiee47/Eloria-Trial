036afaf573226717998272961566791c30f0a417
Raja Shehroz <r.m.shehroz047@gmail.com>
2026-06-28 23:34:26 +0500

    egfjfe

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,11 +1,20 @@
 use tauri::Emitter;
-use tauri::Listener;
 
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
   tauri::Builder::default()
     .plugin(tauri_plugin_shell::init())
     .plugin(tauri_plugin_deep_link::init())
+    .plugin(tauri_plugin_single_instance::init(|app, args, _cwd| {
+      if let Some(window) = app.get_webview_window("main") {
+        window.set_focus().ok();
+      }
+      for arg in &args {
+        if arg.starts_with("eloria://") {
+          app.emit("deep-link", arg.clone()).ok();
+        }
+      }
+    }))
     .setup(|app| {
       if cfg!(debug_assertions) {
         app.handle().plugin(
@@ -15,11 +24,16 @@ pub fn run() {
         )?;
       }
 
-      let handle = app.handle().clone();
-      app.listen("deep-link://new-url", move |event| {
-        let url = event.payload().to_string();
-        handle.emit("deep-link", url).ok();
-      });
+      #[cfg(desktop)]
+      {
+        use tauri_plugin_deep_link::DeepLinkExt;
+        let handle = app.handle().clone();
+        app.deep_link().on_open_url(move |event| {
+          for url in event.urls() {
+            handle.emit("deep-link", url.to_string()).ok();
+          }
+        });
+      }
 
       Ok(())
     })
